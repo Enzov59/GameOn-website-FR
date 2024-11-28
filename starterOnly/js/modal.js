38c487fccf5e935da8598a7f62e9f91240b003a3
Enzo <enzo.verheggen@gmail.com>
2024-11-28 15:40:13 +0100

    Ajout du message d'acceptation de demande

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -13,16 +13,58 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
 const modalSubmit = document.querySelector(".btn-submit");
+const modalConfirmation = document.querySelector(".modal-confirmation");
+const modalForm = document.querySelector(".modal-form");
+const modalConfirmationClose = document.querySelector(".close-confirmation");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach(btn => btn.addEventListener("click", launchModal));
+
+// close modal event listener
+modalClose.addEventListener("click", closeModal);
+modalConfirmationClose.addEventListener("click", closeModal);
+
+// submit modal
+modalSubmit.addEventListener("click", e => {
+  e.preventDefault();
+  if (validateModal()) {
+    showConfirmation();
+  }
+})
+
+// add input event listener
+document.querySelectorAll("input").forEach(element => {
+  element.addEventListener("blur", e => {
+    e.target.reportValidity();
+  })
+});
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal
-modalClose.addEventListener("click", () => {
+// close modal function
+function closeModal() {
   modalbg.style.display = "none";
-})
+  modalForm.style.display = "block";
+  modalConfirmation.style.display = "none";
+}
+
+// validate modal function
+function validateModal() {
+  let modalValidated = true;
+  document.querySelectorAll("input").forEach(element => {
+    if(!element.checkValidity()) {
+      modalValidated = false;
+      element.reportValidity();
+    }
+  });
+  return modalValidated;
+}
+
+// show confirmation function
+function showConfirmation() {
+  modalForm.style.display = "none";
+  modalConfirmation.style.display = "flex";
+}
